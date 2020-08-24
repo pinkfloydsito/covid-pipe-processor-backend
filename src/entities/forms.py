3d98107ffbc5c5c4b45de2665ff2d0e3d3b316f1
Sebastian <ascacere92@gmail.com>
2020-08-24 12:13:23 -0500

    support labels

--- src/entities/forms.py
+++ src/entities/forms.py
@@ -1,3 +1,4 @@
+import re
 from django import forms
 from entities.models import CovidPipe
 
@@ -15,10 +16,27 @@ class CovidPipeForm(forms.ModelForm):
 
         try:
             numbers = range_pipes.split('-')
-            for i in range(int(numbers[0]), int(numbers[1])+1):
-                CovidPipe.objects.create(
-                    name=str(i),
-                    last_movement=last_movement)
+            match_0 = re.match(r"([a-z]+)([0-9]+)", numbers[0], re.I)
+            match_1 = re.match(r"([a-z]+)([0-9]+)", numbers[1], re.I)
+            items_0 = None
+            if match_0:
+                items_0 = match_0.groups()
+
+            items_1 = None
+            if match_1:
+                items_1 = match_1.groups()
+            
+            if match_0 and match_1:
+                for i in range(int(items_0[1]), int(items_1[1])+1):
+                    CovidPipe.objects.create(
+                        name="{}{}".format(items_0[0], str(i)),
+                        last_movement=last_movement)
+                
+            else:
+                for i in range(int(numbers[0]), int(numbers[1])+1):
+                    CovidPipe.objects.create(
+                        name=str(i),
+                        last_movement=last_movement)
 
         except Exception as e:
             raise(e)
